917bdd550ab2fa543e9b84a7257f60d7acfee240
vikramrajurs007 <vikramrajursurs@gmail.com>
2025-08-24 14:11:41 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -17,10 +17,7 @@ app.use(cors())
 //Routes
 app.get('/', (req,res)=>{res.send("Edemy API is working fine!")})
 app.post('/clerk', express.json(), clerkWebhooks)
-app.use('/api/educator', express.json(), educatorRouter);
-app.use('/api/course', express.json(), courseRouter);
-app.use('/api/user', express.json(), userRouter);
-app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
+
 
 //port nummber
 const PORT =process.env.PORT||6000
